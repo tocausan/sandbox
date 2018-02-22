426696e8774af7af17876f6ee4e9e1a02d452291
tocausan <tocausan@gmail.com>
2018-02-22 22:29:12 +0100

    readme

---
+++ readme.md
@@ -0,0 +1,25 @@
+### Sandbox
+
+This sandbox gives the ability to build and test frontend utilities locally.
+It is based on one main menu linking to the added components.
+
+## Installation
+Clone this repo and use NPM to install the packages.
+```
+npm install
+npm start
+```
+
+In order to add a new component you need to add its name to the routes list
+then create a folder with the same name in public containing style, script and view.
+```javascript
+// routes/index.js
+const apps = [
+    'new-app-name'
+    ]
+// public/new-app-name/
+//  main.ejs
+//  main.scss
+//  main.js
+    
+```
--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,47 @@
 const express = require('express'),
-    router = express.Router(),
-    apps = [
-        'index',
-        'k2000',
-        'magic-word',
-        'hello-doodle',
-        'shadow-list',
-        'date-digit',
-        'clock-digit',
-        'speech-synthesis',
-        'pinky',
-        'psychedelik',
-        'distance-computer',
-        'order-form',
-        'command-line',
-        'entity',
-        'intelligent-entity',
-        'cube',
-        'cube-wall',
-        'collision',
-        'square-clock',
-        'cube-inception',
-        'file-uploader'
-    ];
+    router = express.Router();
+
+const apps = [
+    'index',
+    'k2000',
+    'magic-word',
+    'hello-doodle',
+    'shadow-list',
+    'date-digit',
+    'clock-digit',
+    'speech-synthesis',
+    'pinky',
+    'psychedelik',
+    'distance-computer',
+    'order-form',
+    'command-line',
+    'entity',
+    'intelligent-entity',
+    'cube',
+    'cube-wall',
+    'collision',
+    'square-clock',
+    'cube-inception',
+    'file-uploader'
+];
 
 router.get('/', function (req, res, next) {
     res.redirect('index');
 });
 
-apps.forEach((app) => {
+apps.forEach((app) = > {
     router.get('/' + app, function (req, res, next) {
 
-        res.render('template', {
-            title: app,
-            view: '../public/' + app + '/main.ejs',
-            style: '../' + app + '/main.css',
-            script: '../' + app + '/main.js',
-            links: apps.slice(1, apps.length)
-        });
+    res.render('template', {
+        title: app,
+        view: '../public/' + app + '/main.ejs',
+        style: '../' + app + '/main.css',
+        script: '../' + app + '/main.js',
+        links: apps.slice(1, apps.length)
     });
 });
+})
+;
 
 
 module.exports = router;
